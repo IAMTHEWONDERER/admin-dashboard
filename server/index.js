7a3d494df05733542cbc69647e32e2a2636a2203
Oussama Alouche <theoblivionitself@gmail.com>
2024-04-04 11:56:14 +0000

    first commit for Admin DASHBOARD

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import clientRoutes from './routes/clientRoutes';
+import generalRoutes from './routes/generalRoutes.js';
+import managementRoutes from './routes/managementRoutes';
+import salesRoutes from './routes/aslesRoutes.js';
+
+
+
+//configs
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(cors());
+
+
+//routes
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales",salesRoutes);
