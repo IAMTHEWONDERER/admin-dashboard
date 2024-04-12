2e8258a855d3c304843a5215b47a0bad812bba16
Oussama Alouche <theoblivionitself@gmail.com>
2024-04-12 02:51:32 +0000

    finished performance, same code

--- client/src/scenes/performance/index.jsx
+++ client/src/scenes/performance/index.jsx
@@ -41,3 +41,53 @@ const Performance = () => {
       renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
     },
   ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header
+        title="PERFORMANCE"
+        subtitle="Track your Affiliate Sales Performance Here"
+      />
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={(data && data.sales) || []}
+          columns={columns}
+          components={{
+            ColumnMenu: CustomColumnMenu,
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Performance;
