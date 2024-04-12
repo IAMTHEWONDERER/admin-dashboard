2d91b7a00998e7755a6dbd7d2ebbc5cccc45eafc
Oussama Alouche <theoblivionitself@gmail.com>
2024-04-12 02:47:49 +0000

    added performance

---
+++ client/src/scenes/performance/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { useGetUserPerformanceQuery } from "state/api";
+import { useSelector } from "react-redux";
+import { DataGrid } from "@mui/x-data-grid";
+import Header from "components/Header";
+import CustomColumnMenu from "components/DataGridCustomColumnMenu";
+
+const Performance = () => {
+  const theme = useTheme();
+  const userId = useSelector((state) => state.global.userId);
+  const { data, isLoading } = useGetUserPerformanceQuery(userId);
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "CreatedAt",
+      flex: 1,
+    },
+    {
+      field: "products",
+      headerName: "# of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+  ];
